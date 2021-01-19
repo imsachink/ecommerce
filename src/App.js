14571fe3a3b393defeecd1dce8197a48f4c743fa
sachin kaundal <sdev@outlook.in>
2021-01-19 16:14:26 +0530

    some route added and hoc is added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Homepage from "./pages/homepage.component";
+
+import {Switch,Route} from 'react-router-dom';
+
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./homepage.style.scss";
+import Directory from "../components/directory/directory.component"
+
+function Homepage() {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+        
+    )
+}
+
+export default Homepage;
